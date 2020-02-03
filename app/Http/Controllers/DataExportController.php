7e15ba21b90d1318c9e5ae7925160ece870bb75d
Ryo Suzuki <ryo.s12330@gmail.com>
2020-02-03 17:45:27 +0900

    fix windown csv encode

--- app/Http/Controllers/DataExportController.php
+++ app/Http/Controllers/DataExportController.php
@@ -119,6 +119,7 @@ class DataExportController extends Controller
         header('Content-Type: application/octet-stream');
         header("Content-Disposition: attachment; filename=$fileName.csv");
 
-        foreach ($csv as $line) fputcsv(fopen('php://output', 'w'), $line);
+        foreach ($csv as $line) fputcsv(fopen('php://output', 'w'), mb_convert_encoding($line, 'Shift_JIS', 'UTF-8'));
+
     }
 }
