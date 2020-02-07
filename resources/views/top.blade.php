c9696bc2ff7b8d10f5ecef65f9bfb54bb7f9c534
Ryo Suzuki <ryo.s12330@gmail.com>
2020-02-07 14:13:52 +0900

    fix top construction-name width

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -25,51 +25,54 @@
             <div class="refine">
                 <div><span class="refine-title">絞り込み</span></div>
                 <form method="get" action="/" enctype="multipart/form-data">
-                    <select name="userName" data-toggle="select" class="select2 form-control select select-default mrs mbm">
-                        <option value="" label="default">名前を選択</option>
+                    <div>
+                        <select name="userName" data-toggle="select" class="select2 form-control select select-default mrs mbm">
+                            <option value="" label="default">名前を選択</option>
 
-                        @foreach(array_unique($userNames) as $value)
-                            @if($dailyreportsPalams['userName'] == $value)
-                                <option value="{{$value}}" selected>{{$value}}</option>
-                            @else
-                                <option value="{{$value}}">{{$value}}</option>
-                            @endif
-                        @endforeach
-                    </select>
+                            @foreach(array_unique($userNames) as $value)
+                                @if($dailyreportsPalams['userName'] == $value)
+                                    <option value="{{$value}}" selected>{{$value}}</option>
+                                @else
+                                    <option value="{{$value}}">{{$value}}</option>
+                                @endif
+                            @endforeach
+                        </select>
 
-                    <select name="department" data-toggle="select" class="select2 form-control select select-default mrs mbm">
-                        <option value="" label="default">部署を選択</option>
+                        <select name="department" data-toggle="select" class="select2 form-control select select-default mrs mbm">
+                            <option value="" label="default">部署を選択</option>
 
-                        @foreach(array("住宅部", "土木部", "特殊建築部", "農業施設部") as $value)
-                            @if($dailyreportsPalams['department'] == $value)
-                                <option value="{{$value}}" selected>{{$value}}</option>
-                            @else
-                                <option value="{{$value}}">{{$value}}</option>
-                            @endif
-                        @endforeach
-                    </select>
+                            @foreach(array("住宅部", "土木部", "特殊建築部", "農業施設部") as $value)
+                                @if($dailyreportsPalams['department'] == $value)
+                                    <option value="{{$value}}" selected>{{$value}}</option>
+                                @else
+                                    <option value="{{$value}}">{{$value}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
 
-                    <select name="constructionNumber" data-toggle="select" class="form-control select select-default mrs mbm">
-                        <option value="" label="default">工事番号を選択</option>
-                        @foreach ($constructions as $construction)
-                            @if($dailyreportsPalams['constructionNumber'] == $construction->number)
-                                <option value="{{$construction->number}}" selected>{{$construction->number}}</option>
-                            @else
-                                <option value="{{$construction->number}}">{{$construction->number}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-                    <select name="constructionName" data-toggle="select" class="construction-name form-control select select-default mrs mbm">
-                        <option value="" label="default">工事名を選択</option>
-                        @foreach ($constructions as $construction)
-                            @if($dailyreportsPalams['constructionName'] == $construction->name)
-                                <option value="{{$construction->name}}" selected>{{$construction->name}}</option>
-                            @else
-                                <option value="{{$construction->name}}">{{$construction->name}}</option>
-                            @endif
-                        @endforeach
-                    </select>
-
+                    <div>
+                        <select name="constructionNumber" data-toggle="select" class="form-control select select-default mrs mbm">
+                            <option value="" label="default">工事番号を選択</option>
+                            @foreach ($constructions as $construction)
+                                @if($dailyreportsPalams['constructionNumber'] == $construction->number)
+                                    <option value="{{$construction->number}}" selected>{{$construction->number}}</option>
+                                @else
+                                    <option value="{{$construction->number}}">{{$construction->number}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                        <select name="constructionName" data-toggle="select" class="construction-name form-control select select-default mrs mbm">
+                            <option value="" label="default">工事名を選択</option>
+                            @foreach ($constructions as $construction)
+                                @if($dailyreportsPalams['constructionName'] == $construction->name)
+                                    <option value="{{$construction->name}}" selected>{{$construction->name}}</option>
+                                @else
+                                    <option value="{{$construction->name}}">{{$construction->name}}</option>
+                                @endif
+                            @endforeach
+                        </select>
+                    </div>
 
                     <button class="btn btn-primary btn-refine" onClick="location.href='/construction'">決定</button>
                 </form>
