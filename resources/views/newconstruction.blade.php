fdad3e4eba26736eb18ea243213d013ffd6e5c6b
Ryo Suzuki <ryo.s12330@gmail.com>
2020-02-07 10:03:11 +0900

    fix enter submit

--- resources/views/newconstruction.blade.php
+++ resources/views/newconstruction.blade.php
@@ -117,4 +117,15 @@
         </form>
     </div>
 
+    <script>
+        // Enter無効
+        $(document).ready(function () {
+            $('input,textarea[readonly]').not($('input[type="button"],input[type="submit"]')).keypress(function (e) {
+                if (!e) var e = window.event;
+                if (e.keyCode == 13)
+                    return false;
+            });
+        });
+    </script>
+
 @endsection
