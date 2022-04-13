2419169703d1208075d405c18666d59e752fb279
Ryo Suzuki <ryo.s12330@gmail.com>
2022-04-13 09:18:04 +0900

    create ConstructionNumberToConstructionIdOnConstructionsTable logic

--- app/Console/Commands/Maintenances/2022_04_12_075330_constructionNumber_to_construction_id_on_constructions_table.php
+++ app/Console/Commands/Maintenances/2022_04_12_075330_constructionNumber_to_construction_id_on_constructions_table.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands\Maintenances;
 
+use App\Models\Construction;
+use App\Models\Dailyreport;
 use Illuminate\Console\Command;
 
 class ConstructionNumberToConstructionIdOnConstructionsTable extends Command
@@ -10,6 +12,32 @@ class ConstructionNumberToConstructionIdOnConstructionsTable extends Command
 
     public function handle(): void
     {
-        echo 'run constructionNumber_to_construction_id_on_constructions_table:exec';
+        $constructions = $this->getConstructions();
+
+        foreach ($constructions as $constructionId => $constructionNumber) {
+            $this->updateDailyreportConstructionId($constructionId, $constructionNumber);
+        }
+
+        $this->checkSuccess();
+    }
+
+    private function getConstructions(): object
+    {
+        return Construction::pluck('number', 'id')->unique();
+    }
+
+    private function updateDailyreportConstructionId($constructionId, $constructionNumber): void
+    {
+        Dailyreport::where('constructionNumber', $constructionNumber)->update(['construction_id' => $constructionId]);
+    }
+
+    private function checkSuccess(): void
+    {
+        $errorCount = Dailyreport::where('construction_id', 0)->count();
+        if (0 === $errorCount) {
+            print_r('compleate!');
+        } else {
+            print_r($errorCount.' faild');
+        }
     }
 }
