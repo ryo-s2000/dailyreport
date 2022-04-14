bda5c54c3c8f8dcface8782900f673025df3a1a2
Ryo Suzuki <ryo.s12330@gmail.com>
2022-04-14 15:32:06 +0900

    create index on traders table

---
+++ database/migrations/2022_04_14_061710_add_index_to_traders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToTradersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('traders', function (Blueprint $table) {
+            $table->string('name', 512)->change();
+        });
+
+        Schema::table('traders', function (Blueprint $table) {
+            $table->unique(['name', 'department_id'], 'name_and_department_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('traders', function (Blueprint $table) {
+            $table->dropIndex('name_and_department_id');
+        });
+    }
+}
