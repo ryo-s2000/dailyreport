350964db118371d85ad1728e09eb45034ff4831c
Ryo Suzuki <ryo.s12330@gmail.com>
2022-07-03 16:02:07 -0700

    Create 2022_07_03_193038_add_row_color_to_constructions_table.php

---
+++ database/migrations/2022_07_03_193038_add_row_color_to_constructions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('constructions', function (Blueprint $table) {
+            $table->string('row_color')->nullable()->after('remarks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('constructions', function (Blueprint $table) {
+            $table->dropColumn('row_color');
+        });
+    }
+}
